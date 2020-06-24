cd2c726b513a7e0156bb324a526fcc872d618765
Arvinzwt <zhangwentao577614@gmail.com>
2020-06-24 16:08:22 +0800

    添加cors处理跨域

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: ''});
+});
+
+router.post('/login', function (req, res, next) {
+    res.send({
+        title: '标题内容'
+    });
 });
 
 module.exports = router;
