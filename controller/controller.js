99151323fe9f7e10bbec208fdcfa2f30d25ab0f2
Arvinzwt <zhangwentao577614@gmail.com>
2020-08-06 15:14:48 +0800

    定义egg模板

--- controller/controller.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use('/', require('../routes'));
-app.use('/users', require('../routes/users'));
-
-module.exports = app;
--- models/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: 'localhost',
-    user: 'root',
-    password: 'Jx8Rr3&MT',
-    port: 3306,
-    database: 'crm',
-};
