2e3840f0a8f4616c9ef6f999717289aa19a6890c
Arvinzwt <zhangwentao577614@gmail.com>
2020-10-10 09:55:54 +0800

    demo

--- server.js
+++ server.js
@@ -1,43 +1,46 @@
-const WebSocket = require('ws');
-const server = new WebSocket.Server({port: 8080});//端口号不要和客户端端口号重复
-console.log('服务启动成功')
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var SkyRTC = require('skyrtc').listen(server);
+var path = require("path");
 
-let userList = [];// 用户列表，每个登录的用户都会存储在里面
+var port = process.env.PORT || 3000;
+server.listen(port);
 
-let broadcast = mesObj => {// 消息广播，即用户列表里的每个用户都会收到消息
-    userList.forEach(item => {
-        item.send(JSON.stringify({// send只能发送字符串
-            uid: mesObj.uid, //唯一id
-            message: mesObj.message,//发送的信息
-            type: mesObj.type//发送的类型，0是登录，1是发送消息时，2是退出时
-        }));
-    })
-}
+app.use(express.static(path.join(__dirname, 'public')));
 
-server.on('connection', function connection(ws) {//当有用户建立连接时
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
 
-    ws.uid = [(new Date()).getTime(), Math.floor(Math.random() * 10000)].join('_');//由于没有连接数据库，创建随机数id
-    userList.push(ws);//将用户存储到userList
+SkyRTC.rtc.on('new_connect', function(socket) {
+	console.log('创建新连接');
+});
 
-    broadcast({ //发送登录通知
-        uid: ws.uid,
-        message: '欢迎登录',
-        type: 0
-    })
+SkyRTC.rtc.on('remove_peer', function(socketId) {
+	console.log(socketId + "用户离开");
+});
 
-    ws.on('message', message => { //当用户发消息时
-        broadcast({ //转发给所有人
-            uid: ws.uid,
-            message,
-            type: 1
-        })
-    })
+SkyRTC.rtc.on('new_peer', function(socket, room) {
+	console.log("新用户" , socket.id , "加入房间" , room);
+});
 
-    ws.on('close', () => {//当用户退出时
-        broadcast({//转发给所有人
-            uid: ws.uid,
-            message: '已退出',
-            type: 2
-        })
-    })
+SkyRTC.rtc.on('socket_message', function(socket, msg) {
+	console.log("接收到来自" , socket.id , "的新消息：" , msg);
+});
+
+SkyRTC.rtc.on('ice_candidate', function(socket, ice_candidate) {
+	console.log("接收到来自" + socket.id + "的ICE Candidate");
+});
+
+SkyRTC.rtc.on('offer', function(socket, offer) {
+	console.log("接收到来自" + socket.id + "的Offer");
+});
+
+SkyRTC.rtc.on('answer', function(socket, answer) {
+	console.log("接收到来自" + socket.id + "的Answer");
+});
+
+SkyRTC.rtc.on('error', function(error) {
+	console.log("发生错误：" + error.message);
 });
