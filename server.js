0767b304d3dde7ac5962e020e150aa853aadaf97
Arvinzwt <zhangwentao577614@gmail.com>
2020-10-12 10:55:24 +0800

    websocket

--- server.js
+++ server.js
@@ -5,21 +5,10 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/',(req,res)=>{
+    res.send('hello webrtc')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on http://localhost:${port}`)
 })
-
-
-// websocket
-const WebSocket = require('ws');
-const server = new WebSocket.Server({port: 8080});//端口号不要和客户端端口号重复
-
-server.on('connection', function connection(ws) {//当有用户建立连接时
-    ws.on('message', message => { //当用户发消息时
-        ws.send(JSON.stringify({
-            message,
-            type: 'login',
-            success: true,
-        }))
-    })
-});
