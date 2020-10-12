3b55c81219e05253f074e4af52971592da919805
Arvinzwt <zhangwentao577614@gmail.com>
2020-10-12 09:58:20 +0800

    websocket

--- server.js
+++ server.js
@@ -1,48 +1,25 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var SkyRTC = require('skyrtc').listen(server);
-var path = require("path");
-
-var port = process.env.PORT || 3000;
-server.listen(port);
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+const path = require("path");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-	res.sendfile(__dirname + '/index.html');
-});
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`)
+})
 
 
+// websocket
+const WebSocket = require('ws');
+const server = new WebSocket.Server({port: 8080});//端口号不要和客户端端口号重复
 
-SkyRTC.rtc.on('new_connect', function(socket) {
-	console.log('创建新连接');
-});
-
-SkyRTC.rtc.on('remove_peer', function(socketId) {
-	console.log(socketId + "用户离开");
-});
-
-SkyRTC.rtc.on('new_peer', function(socket, room) {
-	console.log("新用户" , socket.id , "加入房间" , room);
-});
-
-SkyRTC.rtc.on('socket_message', function(socket, msg) {
-	console.log("接收到来自" , socket.id , "的新消息：" , msg);
-});
-
-SkyRTC.rtc.on('ice_candidate', function(socket, ice_candidate) {
-	console.log("接收到来自" + socket.id + "的ICE Candidate");
-});
-
-SkyRTC.rtc.on('offer', function(socket, offer) {
-	console.log("接收到来自" + socket.id + "的Offer");
-});
-
-SkyRTC.rtc.on('answer', function(socket, answer) {
-	console.log("接收到来自" + socket.id + "的Answer");
-});
-
-SkyRTC.rtc.on('error', function(error) {
-	console.log("发生错误：" + error.message);
+server.on('connection', function connection(ws) {//当有用户建立连接时
+    ws.on('message', message => { //当用户发消息时
+        ws.send(JSON.stringify({
+            message,
+            type: 'login',
+            success: true,
+        }))
+    })
 });
