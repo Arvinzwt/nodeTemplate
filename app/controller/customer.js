fc149e4ce8378accc73bfff37aa0081d7b11758b
Arvinzwt <zhangwentao577614@gmail.com>
2020-11-09 20:29:49 +0800

    接口设置

---
+++ app/controller/customer.js
@@ -0,0 +1,123 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class CustomerController extends Controller {
+    async leadsSearch() {
+        this.ctx.body = {}
+    }
+
+    async assignCustomer() {
+        this.ctx.body = {}
+    }
+
+    async callCustomer() {
+        this.ctx.body = {}
+    }
+
+    async todayleads() {
+        this.ctx.body = {}
+    }
+
+    async appointleads() {
+        this.ctx.body = {}
+    }
+
+    async mystudent() {
+        this.ctx.body = {}
+    }
+
+    async detail() {
+        this.ctx.body = {}
+    }
+
+    async search() {
+        this.ctx.body = {}
+    }
+
+    async LeadsCallCenterDetail() {
+        this.ctx.body = {}
+    }
+
+    async leadsCallCenterUpdate() {
+        this.ctx.body = {}
+    }
+
+    async addUpdateTrack() {
+        this.ctx.body = {}
+    }
+
+    async getTrackListByStudentid() {
+        this.ctx.body = {}
+    }
+
+    async getTrackListByPagerStudentid() {
+        this.ctx.body = {}
+    }
+
+    async getOwnerRecordByStudentid() {
+        this.ctx.body = {}
+    }
+
+    async getOwnerRecordPagerByStudentid() {
+        this.ctx.body = {}
+    }
+
+    async recommendSearch() {
+        this.ctx.body = {}
+    }
+
+    async recommendDetail() {
+        this.ctx.body = {}
+    }
+
+    async reccustomer() {
+        this.ctx.body = {}
+    }
+
+    async audit() {
+        this.ctx.body = {}
+    }
+
+    async getTags() {
+        this.ctx.body = {}
+    }
+
+    async newTag() {
+        this.ctx.body = {}
+    }
+
+    async updateTagInfo() {
+        this.ctx.body = {}
+    }
+
+    async deleteRegisteredTag() {
+        this.ctx.body = {}
+    }
+
+    async bindingTag() {
+        this.ctx.body = {}
+    }
+
+    async untyingTag() {
+        this.ctx.body = {}
+    }
+
+    async tagChange() {
+        this.ctx.body = {}
+    }
+
+    async getCallCCCenterList() {
+        this.ctx.body = {}
+    }
+
+    async confirm() {
+        this.ctx.body = {}
+    }
+
+    async uploadReport() {
+        this.ctx.body = {}
+    }
+}
+
+module.exports = CustomerController;
