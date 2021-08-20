0fbb6947bf391cd3601a035686b6b1cf8ef6339f
Arvinzwt <zhangwentao577614@gmail.com>
2021-08-20 16:28:07 +0800

    修改时钟效果

---
+++ public/javascripts/electronicScreen.js
@@ -0,0 +1,13 @@
+function Clock(node, config = {}) {
+    this.config = {
+        star_x: config.star_x || 'center',
+        star_y: config.star_x || 'center',
+        cell: config.star_x || 8,
+        long: config.star_x || 2,
+        angle: config.star_x || 45,
+        gap: config.star_x || 1,
+        interval: config.star_x || 10,
+    }
+}
+
+
