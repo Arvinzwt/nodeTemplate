7eecc0446e0e09144fb28d8d39f69a3d3f99d4cd
Arvinzwt <zhangwentao577614@gmail.com>
2021-08-30 14:47:43 +0800

    添加简历模板

--- routes/api/pdf.js
+++ routes/api/pdf.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const {jsPDF} = require("jspdf"); // will automatically load the node version
 const puppeteer = require('puppeteer');
 
 router.post('/pdfCreate', async function (req, res, next) {
@@ -11,13 +10,22 @@ router.post('/pdfCreate', async function (req, res, next) {
     await page.goto(args['url'], {
         waitUntil: 'networkidle2',
     });
-
-    await page.pdf({path: './public/file/hn.pdf', format: 'a4'});
+    await page.pdf({
+        path: `./public/file/${args['fileName']}`,
+        format: 'a4',
+        printBackground: true,//打印背景图片
+        margin: {
+            top: args['top'],
+            bottom: args['bottom'],
+            left: args['left'],
+            right: args['right']
+        },
+    });
 
     await browser.close();
 
     res.send({
-        url: '/file/hn.pdf'
+        url: `/file/${args['fileName']}`,
     })
 })
 
